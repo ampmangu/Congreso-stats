45ed89260d4ec0b91dc9f8bac9dadba8b5b59191
Adrian Marin <adrianmarinportillo@gmail.com>
2022-03-13 21:37:29 +0100

    Added more info for session

--- src/components/session/SessionInfo.tsx
+++ src/components/session/SessionInfo.tsx
@@ -8,8 +8,24 @@ export default function SessionInfo(props: { data: any, s: TFuncReturn<'translat
       <p>{props.data.votacionNumber}</p>
       <h2 className="titleSession">{props.s}</h2>
       <p>{props.data.titulo}</p>
-      <h2 className="titleSession">Subtitulo</h2>
-      <p>{props.data.titulosubgrupo}</p>
+      { (props.data.textoexpediente === '' || props.data.textoexpediente === props.data.titulo) ? <></> : (
+        <>
+          <h2 className="titleSession">Texto Expediente</h2>
+          <p>{props.data.textoexpediente}</p>
+        </>
+      )}
+      { props.data.titulosubgrupo === '' ? <></> : (
+        <>
+          <h2 className="titleSession">Titulo Subgrupo</h2>
+          <p>{props.data.titulosubgrupo}</p>
+        </>
+      )}
+      { props.data.textosubgrupo === '' ? <></> : (
+        <>
+          <h2 className="titleSession">Texto Subgrupo</h2>
+          <p>{props.data.textosubgrupo}</p>
+        </>
+      )}
       <h2 className="dateSession">{props.s1}</h2>
       <p>{props.data.fecha.toString()}</p>
     </>
