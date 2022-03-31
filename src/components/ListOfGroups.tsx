dfc50c72785e9e2d8c770d14b55bc96c196b77d6
Adrian Marin <adrianmarinportillo@gmail.com>
2022-03-31 17:51:14 +0200

    Added filter group by legislatura

--- src/components/ListOfGroups.tsx
+++ src/components/ListOfGroups.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import httpClient from './service/httpClient';
 import '../styles/listOfGroups.scss';
 import LoadingAnimation from './LoadingAnimation';
+import Select from 'react-select';
 
 const ListOfGroups = () => {
   const { t } = useTranslation();
@@ -11,16 +12,38 @@ const ListOfGroups = () => {
     data,
     loading,
   } = httpClient.useFetch(`${httpClient.urlBase!}/groups`);
+  const groupOptions = [
+    { 'value': 'XIV', 'label': 'XIV' },
+    { 'value': 'XIII', 'label': 'XIII' },
+    { 'value': 'XII', 'label': 'XII' },
+    { 'value': 'XI', 'label': 'XI' },
+    { 'value': 'X', 'label': 'X' }
+  ];
+  const [groupValue, setGroupValue] = React.useState(groupOptions[0]);
+  const handleSelectChange = (event: any) => {
+    console.log(event);
+    setGroupValue(event);
+  };
   return (
     <div className="listOfGroups">
-      <h2>{t('list_of_groups')}</h2>
+      <div className="headerSelection">
+        <h2>{t('list_of_groups')}</h2>
+        <Select
+          id="group-select"
+          name="form-field-name"
+          value={groupValue}
+          onChange={handleSelectChange}
+          options={groupOptions}
+        />
+      </div>
       {loading ? <LoadingAnimation />
         : (
           <ul>
-            {data.map((option: string) => (
+            {data.filter((value: any) => value.legislatura === groupValue.value).map((option: any) => (
               <li>
-                <Link to={`/groups/${option.replace(/\s/g, '')}`}>{option}</Link>
+                <Link to={`/groups/${option.grupo.replace(/\s/g, '')}`}>{option.grupo}</Link>
               </li>
+
             ))}
           </ul>
         )}
